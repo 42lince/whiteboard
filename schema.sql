60f3c437fd3a291940685fb6e141bfc1cc269c46
BrookDu <42.duxiaoxi@gmail.com>
2017-05-02 09:04:36 -0400

    add database update

--- schema.sql
+++ schema.sql
@@ -1,2 +1,2 @@
 DROP TABLE IF EXISTS prices;
-CREATE TABLE prices (broker varchar, tenor text, price real, direction text, qty int, firm text, client text, updatedtime text);
+CREATE TABLE prices (broker text, tenor text, price real, direction text, qty int, firm text, client text, updatedtime text);
